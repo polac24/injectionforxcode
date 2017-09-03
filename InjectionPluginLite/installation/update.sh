b5ebcc358b5bb6b50057092b87d7368cf21ecc2c
Bartosz Polaczyk <polac24@gmail.com>
2017-09-03 21:07:53 +0200

    Add xcode breakpoints auto-configurator

---
+++ InjectionPluginLite/installation/update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ 
+ # Backup previous versions
+ cp ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2.xcbkptlist ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2.xcbkptlist~
+
+# update INTests breakpoints
+xsltproc xcode_breakpoints.xsl ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2.xcbkptlist > ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2_new.xcbkptlist
+mv ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2_new.xcbkptlist ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2.xcbkptlist 
